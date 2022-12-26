22ee4a7532c7911edc49e14d2b94c243bd820044
Thor-Herman van Eggelen <44784559+Thor-Herman@users.noreply.github.com>
2022-12-26 12:41:01 +0100

    add bottom padding so all scores are visible

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -5,14 +5,14 @@ const App = () => {
   return (
     <section className="min-h-screen text-white">
       <Aurora />
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center pb-36">
         <div className="w-max pt-24 text-center font-bold">
           <h1 className="py-2 text-5xl">HÆLGA 2023</h1>
           <div className="border-[1px] border-white" />
         </div>
         <TeamsContainer />
       </div>
-      <div className="fixed bottom-0 h-36 w-full">
+      <div className="fixed bottom-0 h-32 w-full">
         <img className="h-full w-full object-cover object-top" src="/snow.png" alt="A snow bank" />
       </div>
     </section>
