85f31abbd30ea2c6e701497adbfa6c1cd742284c
Thor-Herman van Eggelen <44784559+Thor-Herman@users.noreply.github.com>
2023-01-01 15:40:11 +0100

    extract information to own component

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,4 +1,5 @@
 import Aurora from './Aurora';
+import InformationSection from './InformationSection';
 import Snow from './Snow';
 import TeamsContainer from './TeamsContainer';
 import TextBox from './TextBox';
@@ -8,33 +9,14 @@ const App = () => {
     <section className="text-white">
       <Snow />
       <Aurora />
-      <div className="flex min-h-screen flex-col items-center pb-36">
+      <div className="flex min-h-screen flex-col items-center">
         <div className="w-max pt-24 text-center font-bold">
           <h1 className="py-2 text-5xl">HÆLGA 2023</h1>
           <div className="border-[1px] border-white" />
         </div>
         <TeamsContainer />
       </div>
-      <section className="px-4">
-        <TextBox title="REGLER" bgColor="bg-lightBlue">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
-        </TextBox>
-        <TextBox title="FREDAG" bgColor="bg-green">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
-          </p>
-        </TextBox>
-      </section>
+      <InformationSection />
       <div className="fixed bottom-0 h-32 w-full">
         <img className="h-full w-full object-cover object-top" src="/snow.png" alt="A snow bank" />
       </div>
---
+++ frontend/components/InformationSection.tsx
@@ -0,0 +1,43 @@
+import TextBox from './TextBox';
+
+const InformationSection = () => {
+  return (
+    <section className="mb-32 px-4">
+      <TextBox title="REGLER" bgColor="bg-lightBlue">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+      </TextBox>
+      <TextBox title="FREDAG" bgColor="bg-green">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+      </TextBox>
+      <TextBox title="LØRDAG" bgColor="bg-green">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </TextBox>
+      <TextBox title="SØNDAG" bgColor="bg-green">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat.
+        </p>
+      </TextBox>
+    </section>
+  );
+};
+
+export default InformationSection;
