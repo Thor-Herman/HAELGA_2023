5b95839011b39bd3ec88e5822495485789765fb8
Thor-Herman van Eggelen <44784559+Thor-Herman@users.noreply.github.com>
2023-01-28 17:22:47 +0100

    update info

--- frontend/components/InformationSection.tsx
+++ frontend/components/InformationSection.tsx
@@ -14,27 +14,21 @@ const InformationSection = () => {
       </TextBox>
       <TextBox title="FREDAG" bgColor="bg-green">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+          15:00 (<i>R5</i>) - HÆLGA-kickoff <br />
+          15:30 - Workshops <br />
+          17:30 - Middagsgrupper <br />
         </p>
       </TextBox>
       <TextBox title="LØRDAG" bgColor="bg-green">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          11:30 (<i>Hovedbygget</i>) - Ekspedisjonsløp! <br />
+          13:30 (<i>R5/R8</i>) - Lyntaler <br />
+          16:00 - Bussen kjører til ?̶͓̎?̶̞͋?̸̢̋?̵̈́͜?̴̰͝?̸̧͘ <br />
+          00:30 - Bussen kjører tilbake til Hovudbygget <br />
         </p>
       </TextBox>
       <TextBox title="SØNDAG" bgColor="bg-green">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat.
-        </p>
+        <p>Frivillig HÆNGA med AbaHÆNG ❤️</p>
       </TextBox>
     </section>
   );
--- frontend/components/TeamsContainer.tsx
+++ frontend/components/TeamsContainer.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { onValue, ref } from 'firebase/database';
+import { useEffect, useState } from 'react';
 import { database } from '../firebaseconfig.js';
-import { ref, onValue } from 'firebase/database';
 import TeamBox from './TeamBox';
 
 type Team = {
@@ -16,8 +16,6 @@ const TeamsContainer = () => {
     const teamsRef = ref(db, 'teams');
     onValue(teamsRef, (snapshot) => {
       const data = snapshot.val();
-      console.log(data);
-
       const teams = [];
       for (let key in data) {
         teams.push({ ...data[key], id: key });
@@ -26,17 +24,20 @@ const TeamsContainer = () => {
     });
   }, []);
 
-  console.log(teams);
-
   const teamsJSX = teams
     .sort((a, b) => b.score - a.score)
     .map((team, i) => <TeamBox key={i} index={i} score={team.score} name={team.name} />);
 
   return (
     <div className="relative mt-4 w-full max-w-6xl px-4 sm:px-8 md:px-12">
-      <div className="absolute h-full border-l-[1px] border-white md:border-l-2" />
-      <div className="absolute right-4 h-full border-r-[2px] border-dotted border-red-300 sm:right-8 md:right-12 md:border-l-2" />
-      {teamsJSX}
+      {teams.length > 0 && (
+        <>
+          <div className="absolute h-full border-l-[1px] border-white md:border-l-2" />
+          <div className="absolute right-4 h-full border-r-[2px] border-dotted border-red-300 sm:right-8 md:right-12 md:border-l-2" />
+          {teamsJSX}
+        </>
+      )}
+      {teams.length === 0 && <p className="text-center text-white pt-6">Ingen lag har blitt opprettet ennå. Kom tilbake senere!</p>}
     </div>
   );
 };
